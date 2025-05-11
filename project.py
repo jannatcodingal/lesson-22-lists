9d157b9ad260e8068f550dba64f847967343bf82
jannatcodingal <jannatrehman007@gmail.com>
2025-05-11 18:05:21 +0500

    list project

---
+++ project.py
@@ -0,0 +1,27 @@
+list=[5,2,8,10,23]
+print(list)
+print("square of 1 element is",list[0]**2)
+print("square of 2 element is",list[1]**2)
+print("square of 3 element is",list[2]**2)
+print("square of 4 element is",list[3]**2)
+print("square of 5 element is",list[4]**2)
+if list[0]%2==0:
+    print("the element is even")
+else:
+    print("the element is odd")
+if list[1]%2==0:
+    print("the element is even")
+else:
+    print("the element is odd")
+if list[2]%2==0:
+    print("the element is even")
+else:
+    print("the element is odd")
+if list[3]%2==0:
+    print("the element is even")
+else:
+    print("the element is odd")
+if list[4]%2==0:
+    print("the element is even")
+else:
+    print("the element is odd")
